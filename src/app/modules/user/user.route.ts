1f5a3c1018805915a5281f61879a654642286401
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-10-07 10:55:12 +0300

    added all-code

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
 
-import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
-import { Role } from "./user.interface";
-import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
-import { checkAuth } from "../../middlewares/checkAuth";
 
-const router = Router()
+const router = Router();
 
+router.post("/register", UserControllers.createUser);
+router.get("/all-users", UserControllers.getAllUsers);
+router.get("/me", UserControllers.getMe);
+router.get("/:id", UserControllers.getSingleUser);
 
-
-router.post("/register",
-    validateRequest(createUserZodSchema),
-    UserControllers.createUser)
-router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
-router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
-router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser)
-router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
 // /api/v1/user/:id
-export const UserRoutes = router
+export const UserRoutes = router;
